9af69679ca98a84954dfb1db5f9b458ad26630b0
ediaz23 <esteban.diazt23@gmail.com>
2026-07-18 18:55:29 -0400

    [FIX] prod version

--- src/pre-worker.js
+++ src/pre-worker.js
@@ -1,5 +1,16 @@
 /* global updateMemoryViews:writable wasmMemory */
 
+/** worker fix */
+// eslint-disable-next-line no-unused-vars
+function assert(c, m) {
+    if (!c) {
+        throw m
+    }
+}
+
+// eslint-disable-next-line no-unused-vars
+let asm = null;
+
 // Emscripten does not export the memory views on the Module object by
 // default (HEAPU8 stays as a closure-local var). Hook the internal
 // updateMemoryViews so worker code can read `self.HEAPU8` regardless.
